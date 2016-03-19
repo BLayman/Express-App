619230e0fe47407119b8fda53b573234ae0c533b
Brett <Leasure57@gmail.com>
2016-03-19 22:58:05 +0000

    added test, mocha, and supertest

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 var express = require("express");
 var app = express();
 app.get('/',function(req,res){
-    res.send("OK\n");
-})
 
-app.listen(3000);
+res.send("OK\n");
+});
+
+module.exports = app;
